d83b5f82bcddd75e70a2bd514662fdf66d5d0849
Nicolas <bigmacnls@gmail.com>
2024-08-21 17:24:50 -0300

    ficha

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import ClassesGrid from './pages/ClassesGrid';
+import Ficha from './pages/Ficha';
 
 export default function Home() {
   return (
     <div className="App">
       <ClassesGrid />
+      <Ficha />
     </div>
   );
 }
