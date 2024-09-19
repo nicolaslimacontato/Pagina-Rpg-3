39de6abe22a76db85e2525043c220af6479f8e4c
Nicolas Lima <bigmacnls@gmail.com>
2024-09-19 19:22:37 -0300

    Update interfaces.ts

--- app/interfaces.ts
+++ app/interfaces.ts
@@ -1,29 +1,30 @@
 export interface Classe {
+  nome: string;
+  dadoDeVida: string;
+  vidaInicial: (modConstituicao: number) => number;
+  vidaPorLevel: (modificador: number) => number;
+  ca?: (modifiers: Modifiers) => number;
+  proficiencias: {
+    armaduras: string[];
+    armas: string[];
+    ferramentas: string[];
+    testesDeResistencia: string[];
+    pericias: string[];
+  };
+  equipamento: string[][];
+  habilidades: {
     nome: string;
-    dadoDeVida: string;
-    vidaInicial: number;
-    vidaPorLevel: string;
-    proficiencias: {
-      armaduras: string[];
-      armas: string[];
-      ferramentas: string[];
-      testesDeResistencia: string[];
-      pericias: string[];
-    };
-    equipamento: string[][];
+    descricao: string;
+    nivel: number;
+  }[];
+  subclasses: {
+    nome: string;
+    descricao: string;
     habilidades: {
       nome: string;
       descricao: string;
       nivel: number;
     }[];
-    subclasses: {
-      nome: string;
-      descricao: string;
-      habilidades: {
-        nome: string;
-        descricao: string;
-        nivel: number;
-      }[];
-    }[];
-  }
+  }[];
+};
   
